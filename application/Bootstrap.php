73712008918bad1828c7d2721ad352e7013fae9f
Manuel Manelli <manuelmanelli@icloud.com>
2019-05-22 17:10:48 +0200

    Revert "Implementazione data logger"
    This reverts commit daea750e0420c3cb58a511e89c4f3e17200292b4.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,14 +3,6 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
-  protected $_logger;
-
-  protected function _initLogging(){
-    $writer = new Zend_Log_Writer_Stream(APPLICATION_PATH . '/data/log/log.log');
-    $logger = new Zend_Log($writer);
-    Zend_Registry::set('log', $logger);
-    $this->_logger = $logger;
-    $logger->info('Bootstrap ' .__METHOD__);
-  }
 
 }
+
