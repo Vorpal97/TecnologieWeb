fc4bc0f6f76e06b77eab82c52f47ca3624429882
Manuel Manelli <manuelmanelli@icloud.com>
2019-05-23 18:03:06 +0200

    inizializzazione view nella fase di bootstrap
    Co-Authored-By: Simone Cappella <SimoKappa@users.noreply.github.com>

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -13,4 +13,28 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     $this->_logger = $logger;
     $logger->info('Bootstrap ' .__METHOD__);
   }
+  protected function _initRequest()
+// Aggiunge un'istanza di Zend_Controller_Request_Http nel Front_Controller
+// che permette di utilizzare l'helper baseUrl() nel Bootstrap.php
+// Necessario solo se la Document-root di Apache non è la cartella public/
+{
+    $this->bootstrap('FrontController');
+    $front = $this->getResource('FrontController');
+    $request = new Zend_Controller_Request_Http();
+    $front->setRequest($request);
+}
+
+protected function _initViewSettings()
+{
+    $this->bootstrap('view');
+    $this->_view = $this->getResource('view');
+    $this->_view->headMeta()->setCharset('UTF-8');
+    $this->_view->headMeta()->appendHttpEquiv('Content-Language', 'it-IT');
+    $this->_view->headMeta()->appendHttpEquiv('name', 'viewport');
+    $this->_view->headLink()->appendStylesheet($this->_view->baseUrl('/layout/styles/layout.css'));
+    $this->_view->headTitle('IZY Car rent');
+}
+
+
+
 }
