a300512e2af6e81ccb36f2d1c639db881c35d855
Simone Cappella <48387175+SimoKappa@users.noreply.github.com>
2019-05-28 18:01:19 +0200

    Modificato View script per la registerAction e modificato il decorator pattern astratto

--- library/App/Form/Abstract.php
+++ library/App/Form/Abstract.php
@@ -7,15 +7,15 @@ class App_Form_Abstract extends Zend_Form
 		array(array('alias2' => 'HtmlTag'), array('tag' => 'td', 'class' => 'errors','openOnly' => true, 'placement' => 'append')),
 		'Errors',
 		array(array('alias3' => 'HtmlTag'), array('tag' => 'td', 'closeOnly' => true, 'placement' => 'append')),
-        array('Label', array('tag' => 'td')),
+        array('Label', array('tag' => 'td', 'style' => 'width:130px;float:right;')),
         array(array('alias4' => 'HtmlTag'), array('tag' => 'tr')),
         );
-    
+
     public $buttonDecorators = array(
         'ViewHelper',   //genera il button
         array(array('button1' => 'HtmlTag'),array('tag' => 'td')),
         array(array('button2' => 'HtmlTag'), array('tag' => 'td', 'class' => 'button')),
         array(array('button3' => 'HtmlTag'),array('tag' => 'tr')),
     );
-    
+
 }
