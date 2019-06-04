c3563b87d2355c18c8b2f68ae8042a024df44530
Simone Cappella <48387175+SimoKappa@users.noreply.github.com>
2019-06-04 17:28:37 +0200

    Creato Staff controller

--- docs/database_scripts/tweb_test_dataset.sql
+++ docs/database_scripts/tweb_test_dataset.sql
@@ -1,13 +1,9 @@
-﻿TRUNCATE TABLE auto;
-
-INSERT INTO auto
+﻿INSERT INTO auto 
   (targa,marca,modello,segmento,alimentazione,cilindrata,potenza,cavalli,prezzo,colore,n_posti,immagine)
 VALUES
   ("FJ123XA", "Ferrari", "La Ferrari", "Supercar", "Benzina", 6200, 440, 800, 3000, "Rosso", 2, "laferrari.jpg"),
   ("FR456ZA", "Mercedes-Benz", "GT4", "Supercar", "Benzina", 4000, 360, 510, 2300, "Nero", 2, "gt4.jpg"),
   ("FN789FX", "Maserati", "Levante", "SUV", "Diesel", 3000, 257,350, 2000, "Blu notte", 5, "levante.jpg");
-
-TRUNCATE TABLE utente;
 
 INSERT INTO utente
   (nome, cognome, email, psw, data, autenticazione, residenza, occupazione)
@@ -16,23 +12,17 @@ VALUES
   ("Luigi", "Verdi", "luigiv@ymail.com", "luve", STR_TO_DATE('14-09-1977', '%d-%m-%Y'),1, 'Milano', 'lavoratore'),
   ("Admin", "Admin", "NA", "admin", STR_TO_DATE('1-01-1968', '%d-%m-%Y'),1, 'Roma', 'lavoratore');
 
-TRUNCATE TABLE prenotazione;
-
 INSERT INTO prenotazione
   (id_utente,id_auto,data_inizio,data_fine)
 VALUES
   (1,3,STR_TO_DATE('24-05-2019', '%d-%m-%Y'),STR_TO_DATE('27-05-2019', '%d-%m-%Y')),
   (2,1,STR_TO_DATE('20-05-2019', '%d-%m-%Y'),STR_TO_DATE('26-05-2019', '%d-%m-%Y'));
 
-TRUNCATE TABLE messaggio;
-
 INSERT INTO messaggio
   (id_mittente,id_destinatario,corpo)
 VALUES
   (1,3,"Salve, avrei la necessità che la macchina sia dotata di pneumatici invernali"),
   (3,1,"Salve a lei, le nostre vetture sono al momento dotate tutte di pneumatici adatti alla stagione in corso");
-
-TRUNCATE TABLE faq;
 
 INSERT INTO faq
   (domanda,risposta)
