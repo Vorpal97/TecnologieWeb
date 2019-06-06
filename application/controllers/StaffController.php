f754b4fd24b74242084c05ec96f6ed37539c9ab9
Simone Cappella <48387175+SimoKappa@users.noreply.github.com>
2019-06-06 10:53:11 +0200

    Logout action per staff

--- application/controllers/StaffController.php
+++ application/controllers/StaffController.php
@@ -6,11 +6,18 @@ class StaffController extends Zend_Controller_Action
     public function init()
     {
         $this->_helper->layout->setLayout('staff');
+        $this->_authService = new Application_Service_Auth();
     }
 
     public function indexAction()
     {
         // action body
+    }
+    
+        public function logoutAction()
+    {
+        $this->_authService->logout();
+	return $this->_helper->redirector('index','public');	
     }
 
 
