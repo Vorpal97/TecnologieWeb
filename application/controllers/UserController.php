4195b3ceff9381da2638d682da0d6eb02cad3ab5
Danitoms <49287565+Danitoms@users.noreply.github.com>
2019-06-10 12:24:53 +0200

    risolto il problema dei 4 salvataggi nel db

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -1,69 +1,63 @@
 <?php
 
-class UserController extends Zend_Controller_Action
-{
+class UserController extends Zend_Controller_Action {
+
     protected $_catalogModel = null;
     protected $_reservationModel = null;
     protected $_authService = null;
 
-    public function init()
-    {
-      $this->_helper->layout->setLayout('main');
-      $this->_authService = new Application_Service_Auth();
-      $this->view->livello = $this->_authService->getIdentity()->autenticazione;
-      $this->_catalogModel = new Application_Model_Catalog();
-      $this->_reservationModel = new Application_Model_Reservation();
-      $this->view->id_utente = $this->_authService->getIdentity()->id_utente;
+    public function init() {
+        $this->_helper->layout->setLayout('main');
+        $this->_authService = new Application_Service_Auth();
+        $this->view->livello = $this->_authService->getIdentity()->autenticazione;
+        $this->_catalogModel = new Application_Model_Catalog();
+        $this->_reservationModel = new Application_Model_Reservation();
+        $this->view->id_utente = $this->_authService->getIdentity()->id_utente;
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         $this->view->azione = $this->getRequest()->getActionName();
     }
 
-    public function logoutAction()
-    {
+    public function logoutAction() {
         $this->_authService->logout();
-	return $this->_helper->redirector('index','public');
+        return $this->_helper->redirector('index', 'public');
     }
 
-    public function profiloAction()
-    {
+    public function profiloAction() {
         $this->view->azione = $this->getRequest()->getActionName();
         // action body
     }
 
-    public function editaprofiloAction()
-    {
+    public function editaprofiloAction() {
         // action body
     }
-    
-    public function preventivoAction()
-    {
-        
-        
+
+    public function preventivoAction() {
+
+
         $idAuto = $this->_getParam('idAuto', null);
         $inizio = $this->_getParam('dataInizio');
         $fine = $this->_getParam('dataFine');
-        
-        if(!is_null($idAuto)){
+
+        if (!is_null($idAuto)) {
             $prods = $this->_catalogModel->getAutoById($idAuto);
         }
-        if ($inizio!=0 && $fine!=0){
+        if ($inizio != 0 && $fine != 0) {
             $pren = $this->_reservationModel->getPrenotazioneByAuto($idAuto);
         }
         $this->view->assign(array('products' => $prods, 'prenotazioni' => $pren));
     }
-    
-    public function prenotazioneAction (){
-        
+
+    public function prenotazioneAction() {
+
         $dataInizio = $this->_getParam('data_inizio', null);
-        $dataFine  = $this->_getParam('data_fine', null);
+        $dataFine = $this->_getParam('data_fine', null);
         $idAuto = $this->_getParam('id_auto', null);
         $idUtente = $this->_getParam('id_utente', null);
-        
-        $this->_reservationModel->setPrenotazione(array('data_inizio' => $dataInizio, 'data_fine' => $dataFine, 'id_auto' => $idAuto, 'id_utente' => $idUtente));
+        if ($idUtente != 0) {
+            $this->_reservationModel->setPrenotazione(array('data_inizio' => $dataInizio, 'data_fine' => $dataFine, 'id_auto' => $idAuto, 'id_utente' => $idUtente));
+        }
         $this->view->assign(array('data_inizio' => $dataInizio, 'data_fine' => $dataFine, 'id_auto' => $idAuto, 'id_utente' => $idUtente));
-        
     }
 }
