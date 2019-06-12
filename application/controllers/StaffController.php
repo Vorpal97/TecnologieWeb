31a0ae21e2850ac2c6c41453b351e3e3fcef04a1
Simone Cappella <48387175+SimoKappa@users.noreply.github.com>
2019-06-12 18:56:39 +0200

    Modifiche cucchiarelli per l'update

--- application/controllers/StaffController.php
+++ application/controllers/StaffController.php
@@ -39,7 +39,7 @@ class StaffController extends Zend_Controller_Action {
         $values = $form->getValues();
         $autoid = $this->getParam('idauto', null);
         $this->_staffModel->editAuto($values, $autoid);
-        $this->_helper->redirector('modifica');
+        $this->_helper->redirector('catalog', 'public');
     }
 
     public function addautoAction() {
@@ -63,32 +63,20 @@ class StaffController extends Zend_Controller_Action {
         $this->view->azione = $this->getRequest()->getActionName();
     }
 
-    public function modificaAction() {
+    public function modificaAction()
+    {
         $this->view->azione = $this->getRequest()->getActionName();
         $autoid = $this->_getParam('idauto', null);
         $this->view->idauto = $autoid;
         $urlHelper = $this->_helper->getHelper('url');
         $data = $this->_catalogModel->getAutoById($autoid);
-        $precompiled = array(
-            "marca" => $data[0]->marca,
-            "modello" => $data[0]->modello,
-            "targa" => $data[0]->targa,
-            "segmento" => $data[0]->segmento,
-            "alimentazione" => $data[0]->alimentazione,
-            "immagine" => $data[0]->immagine,
-            "cilindrata" => $data[0]->cilindrata,
-            "potenza" => $data[0]->potenza,
-            "cavalli" => $data[0]->cavalli,
-            "prezzo" => $data[0]->prezzo,
-            "colore" => $data[0]->colore,
-            "n_posti" => $data[0]->n_posti,
-        );
-        $this->_form->populate($precompiled);
+        $this->_form->populate($data->toArray());
         $this->_form->setAction($urlHelper->url(array(
-                    'controller' => 'staff',
-                    'action' => 'updateauto',
-                    'idauto' => $data[0]->id_auto), 'default'
-        ));
+        'controller' => 'staff',
+        'action' => 'updateauto',
+        'idauto' => $autoid),
+        'default'
+         ));
         return $this->_form;
     }
 
