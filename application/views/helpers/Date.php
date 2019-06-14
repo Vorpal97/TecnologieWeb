ace48dc6d20043774df25d63a94866d309824377
Danitoms <49287565+Danitoms@users.noreply.github.com>
2019-06-14 11:35:45 +0200

    fix alla prenotazione

--- application/views/helpers/Date.php
+++ application/views/helpers/Date.php
@@ -8,13 +8,13 @@ class Zend_View_Helper_Date extends Zend_View_Helper_Abstract {
 
         if ($in < $oggi && $in != 0) {
             $res = '<p style="text-align: center;">Non siamo autorizzati a tornare indietro nel tempo</p><p style="text-align: center">La data di inizio inserita è: ' . $in . '</p><p style="text-align: center">Controllare il calendario e sceglierne un\'altra';
-            $c += 1;
+
         } else if ($in > $fin) {
             $res = '<p style="text-align: center;">La data di fine precede la data di inizio</p><p style="text-align: center;">Inserire le date nel giusto ordine</p>';
-            $c += 1;
+
         } else if ($in == 0 || $fin == 0) {
             $res = '<p style="text-align: center;">Inserire la data di inizio e fine desiderate<p>';
-            $c += 1;
+
         } else {
             $res = 1;
         }
