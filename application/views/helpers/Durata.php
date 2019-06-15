bbcd15e459c2a4bef5d46f92bac79ee7ef4f548e
Danitoms <49287565+Danitoms@users.noreply.github.com>
2019-06-15 17:12:50 +0200

    qualche piccola formattazione

--- application/views/helpers/Durata.php
+++ application/views/helpers/Durata.php
@@ -5,7 +5,8 @@ class Zend_View_Helper_Durata extends Zend_View_Helper_Abstract {
     public function durata($inizio, $fine) {
         $a = strtotime($inizio);
         $b = strtotime($fine);
-        $durata = 1+(($b - $a) / 3600) / 24;
+        $durata = 1 + (($b - $a) / 3600) / 24;
         return $durata;
     }
+
 }
